3d6403124e9e7cc68a254376c7cf4f80073a51ad
Antwone <antwone@gmail.com>
2019-03-20 20:16:41 -0400

    initial commit

---
+++ git_init.bash
@@ -0,0 +1,6 @@
+echo "# simple-rest-api" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:AntwoneDW/simple-rest-api.git
+git push -u origin master
